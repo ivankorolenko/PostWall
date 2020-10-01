be148b97fefab954c8b505961fac3428b451e4ef
ivankorolenko <korolenko.i.u@gmail.com>
2020-10-01 22:13:23 +0300

    set states and events, refactored some components to OOP and set usefull libs(reactstrap and styled-components)

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {Component} from 'react';
 
 import AppHeader from '../app-header';
 import SearchPanel from '../search-panel';
@@ -7,19 +7,72 @@ import PostList from '../post-list';
 import PostAddForm from '../post-add-form';
 
 import './app.css';
+import styled from 'styled-components';
 
-const App = () => {
-    return (
-        <div className="app">
-            <AppHeader/>
-            <div className="search-panel d-flex">
-                <SearchPanel/>
-                <PostStatusFilter/>
-            </div>
-            <PostList/>
-            <PostAddForm/>
-        </div>
-    )
-}
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`;
 
-export default App;
+export default class App extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            data: [
+                {label: "Going to learn React", important: true, id: 1},
+                {label: "Continue learning", important: false, id: 2},
+                {label: "Almost finished", important: false, id: 3}
+            ]
+        };
+
+        this.maxId = 4;
+
+        this.deleteItem = this.deleteItem.bind(this);
+        this.addItem = this.addItem.bind(this);
+    }
+
+    deleteItem(id) {
+        this.setState(({data}) => {
+            const index = data.findIndex(elem => elem.id === id),
+                  newArr = [...data.slice(0, index), ...data.slice(index + 1)];
+
+                  return {
+                      data: newArr
+                  }
+        })
+    }
+
+    addItem(body) {
+        const newItem = {
+            label: body,
+            important: false,
+            id: this.maxId++
+        };
+
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            
+            return {
+                data: newArr
+            }
+        });
+    }
+
+    render() {
+        return (
+            <AppBlock>
+                <AppHeader/>
+                <div className="search-panel d-flex">
+                    <SearchPanel/>
+                    <PostStatusFilter/>
+                </div>
+                <PostList 
+                posts={this.state.data}
+                onDelete={this.deleteItem}/>
+                <PostAddForm
+                onAdd={this.addItem}/>
+            </AppBlock>
+        )
+    }
+} 
--- src/components/search-panel/search-panel.js
+++ src/components/search-panel/search-panel.js
@@ -4,9 +4,9 @@ import './search-panel.css';
 const SearchPanel = () => {
     return (
         <input
-            className="form-control search-input"
-            type="text"
-            placeholder="Поиск по записям"
+        className="form-control search-input"
+        type="text"
+        placeholder="Поиск по записям"
         />
     )
 }
