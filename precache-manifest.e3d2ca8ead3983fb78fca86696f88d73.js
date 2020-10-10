78dbdc50d684b9f34aa1d19fe150b147171dc15d
ivankorolenko <korolenko.i.u@gmail.com>
2020-10-10 17:56:49 +0300

    Updates

---
+++ precache-manifest.e3d2ca8ead3983fb78fca86696f88d73.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ee2b0c2769ba40197255e97a4784bf7",
+    "url": "/PostWall___React/index.html"
+  },
+  {
+    "revision": "5a47c16e7fb30aa3f491",
+    "url": "/PostWall___React/static/css/2.8aa5a7f8.chunk.css"
+  },
+  {
+    "revision": "f952414bfe67cf13dcf0",
+    "url": "/PostWall___React/static/css/main.d9e6a227.chunk.css"
+  },
+  {
+    "revision": "5a47c16e7fb30aa3f491",
+    "url": "/PostWall___React/static/js/2.b925ec25.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PostWall___React/static/js/2.b925ec25.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f952414bfe67cf13dcf0",
+    "url": "/PostWall___React/static/js/main.a7505aae.chunk.js"
+  },
+  {
+    "revision": "438dfbd7d62d8ce78269",
+    "url": "/PostWall___React/static/js/runtime-main.6b2b5eb5.js"
+  }
+]);
